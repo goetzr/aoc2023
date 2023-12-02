d19b9cda897cdb61c7c9aa458526f43ebeddf375
Russ Goetz <russgoetz@gmail.com>
2023-12-01 20:58:43 -0500

    Working on day 1 part 2

--- aoc01/src/main.rs
+++ aoc01/src/main.rs
@@ -26,3 +26,33 @@ fn part2(input: &str) -> Result<()> {
     writeln!(io::stdout(), "{}", input)?;
     Ok(())
 }
+
+fn to_number(data: &str) -> Option<(u32, &str)> {
+    if let Some(digit) = data.chars().next().expect("empty data").to_digit(10) {
+        // What if this takes us to the end?
+        Some(digit, &data[1..]
+    }
+}
+
+fn to_number<I: Iterator<Item=char> + Clone>(chars: Box<I>) -> Option<(u32, Box<I>)> {
+    Some((7, chars.clone()))
+    //unimplemented!();
+}
+
+fn get_spelled_out_num(chars: &impl Iterator<Item=char>) -> Option<u32> {
+    if chars.clone().take()
+}
+
+fn spelled_out(num: i32) -> &'static str {
+    match num {
+        1 => "one",
+        2 => "two",
+        3 => "three",
+        4 => "four",
+        5 => "five",
+        6 => "six",
+        7 => "seven",
+        8 => "eight",
+        9 => "nine",
+    }
+}
