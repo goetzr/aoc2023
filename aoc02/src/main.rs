8b7a7c23c9e4ccc41e9f4dd427370326d0d4c927
Russ Goetz <russgoetz@gmail.com>
2023-12-04 06:07:38 -0500

    Working on day 2 part 1

--- aoc02/src/main.rs
+++ aoc02/src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{self, Read, Write};
+use std::io::{self, Write};
 
 type Error = dyn std::error::Error + 'static;
 type Result<T> = std::result::Result<T, Box<Error>>;
@@ -10,9 +10,51 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn part1(input: &str) -> Result<()> {
-    writeln!(io::stdout(), "{}", input)?;
-    Ok(())
+struct Trial {
+    red: u32,
+    green: u32,
+    blue: u32,
+}
+
+impl Trial {
+    fn new() -> Self {
+        Trial { red: 0, green: 0, blue: 0 }
+    }
+}
+
+fn parse_line(line: &str) -> Result<Vec<Trial>> {
+    let (game_id, trials_str) = line.split_once(':').ok_or("missing colon")?;
+
+    let (_, game_id) = game_id.split_once(' ').ok_or("missing space in game label")?;
+    // TODO: How to label this error?
+    let game_id = game_id.parse::<u32>()?;
+
+    let mut trials = Vec::new();
+    let trials_strs = trials_str.split(';').collect::<Vec<_>>();
+    for trial in trials_strs {
+        let cube_choices = trial.split(',').collect::<Vec<_>>();
+        let mut trial = Trial::new();
+        for cube_choice in cube_choices {
+            let (number, color) = cube_choice.split_once(' ').ok_or("missing space in cube choice")?;
+            match color {
+                "red" => 
+            }
+        }
+    }
+    
+    unimplemented!()
+}
+mod part1 {
+    use super::*;
+
+    fn part1(input: &str) -> Result<()> {
+        for line in input.lines() {
+
+        }
+        writeln!(io::stdout(), "{}", input)?;
+        Ok(())
+    }
+
 }
 
 fn part2(input: &str) -> Result<()> {
