7b1b578b65a1524036d6d5526f3275e8783459d4
Russ Goetz <russgoetz@gmail.com>
2023-12-06 06:00:29 -0500

    Working day 3 part 1

--- aoc03/src/main.rs
+++ aoc03/src/main.rs
@@ -1,21 +1,45 @@
-use std::io::{self, Read, Write};
+use std::io::{self, Write};
+use anyhow::{Context};
 
-type Error = dyn std::error::Error + 'static;
-type Result<T> = std::result::Result<T, Box<Error>>;
 
-fn main() -> Result<()> {
+fn main() -> anyhow::Result<()> {
     let input = io::read_to_string(io::stdin())?;
-    part1(&input)?;
-    part2(&input)?;
+    part1::part1(&input)?;
+    part2::part2(&input)?;
     Ok(())
 }
 
-fn part1(input: &str) -> Result<()> {
-    writeln!(io::stdout(), "{}", input)?;
-    Ok(())
+struct Window<'a, I: Iterator<Item = &'a str>> {
+    iter: I,
+    lines: [&'a u8; 3],
 }
 
-fn part2(input: &str) -> Result<()> {
-    writeln!(io::stdout(), "{}", input)?;
-    Ok(())
+impl<'a, I: Iterator<Item = &'a str>> Window<'a, I> {
+    fn new(iter: I) -> Self {
+        // TODO: Grab lines. What if there are less than 2 lines?
+        let line1 = iter.next();
+        Window { iter, }
+    }
+    fn slide_down(&mut self) {
+
+    }
+}
+mod part1 {
+    use super::*;
+
+    pub fn part1(input: &str) -> anyhow::Result<()> {
+        writeln!(io::stdout(), "{}", input)?;
+        Ok(())
+    }
+
+}
+
+mod part2 {
+    use super::*;
+
+    pub fn part2(input: &str) -> anyhow::Result<()> {
+        writeln!(io::stdout(), "{}", input)?;
+        Ok(())
+    }
+
 }
