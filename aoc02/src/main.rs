29075da021bcd93e8471c5f451fe561a4d504527
Russ Goetz <russgoetz@gmail.com>
2023-12-05 19:59:33 -0500

    Finished day 2 part 1

--- aoc02/src/main.rs
+++ aoc02/src/main.rs
@@ -1,19 +1,47 @@
-use anyhow::{anyhow, ensure, Context};
+use anyhow::{anyhow, bail, ensure, Context};
 use std::collections::HashSet;
 use std::io::{self, Write};
 
 fn main() -> anyhow::Result<()> {
     let input = io::read_to_string(io::stdin())?;
-    part1(&input)?;
+    part1::part1(&input)?;
     part2(&input)?;
     Ok(())
 }
 
+#[derive(Debug)]
 struct Game {
     id: u32,
     trials: Vec<Trial>,
 }
 
+impl Game {
+    fn parse(line: &str) -> anyhow::Result<Game> {
+        //    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+        let (game_label, trials) = line.split_once(':').ok_or(anyhow!("missing colon"))?;
+
+        let (_, game_id) = game_label
+            .split_once(' ')
+            .ok_or(anyhow!("missing space in game label"))?;
+        let game_id = game_id
+            .parse::<u32>()
+            .context("failed to parse game ID from game label")?;
+
+        let trial_strs = trials.split(';').collect::<Vec<_>>();
+        let mut trials = Vec::new();
+        for (trial_idx, trial) in trial_strs.iter().enumerate() {
+            trials.push(Trial::parse(game_id, trial_idx, trial)?);
+        }
+
+        let game = Game {
+            id: game_id,
+            trials,
+        };
+        Ok(game)
+    }
+}
+
+#[derive(Debug)]
 struct Trial {
     red: u32,
     green: u32,
@@ -22,20 +50,23 @@ struct Trial {
 
 impl Trial {
     fn parse(game_id: u32, trial_idx: usize, trial: &str) -> anyhow::Result<Self> {
-        let msg = |msg: &str| format!("game ID: {}, trial index: {}: {}", game_id, trial_idx, msg);
+        //    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+        let msg = |msg: &str| format!("game ID = {}, trial index = {}: {}", game_id, trial_idx, msg);
 
-        let mut trial = Trial {
-            red: 0,
-            green: 0,
-            blue: 0,
-        };
         let mut colors_seen = HashSet::new();
         let cubes = trial.split(',').collect::<Vec<_>>();
         ensure!(
             cubes.len() > 0,
             msg("at least 1 cube choice must be present in a trial")
         );
+
+        let mut trial = Trial {
+            red: 0,
+            green: 0,
+            blue: 0,
+        };
         for cube in cubes {
+            let cube = cube.trim();
             let (number, color) = cube
                 .split_once(' ')
                 .ok_or(anyhow!(msg("cube choice not space-separated")))?;
@@ -52,43 +83,35 @@ impl Trial {
                 "red" => trial.red = number,
                 "green" => trial.green = number,
                 "blue" => trial.blue = number,
+                _ => bail!(msg("invalid color in cube choice")),
             };
         }
         Ok(trial)
     }
 }
-
-fn parse_line(line: &str) -> anyhow::Result<Vec<Game>> {
-    //    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-    let (game_label, trials) = line.split_once(':').ok_or(anyhow!("missing colon"))?;
-
-    let (_, game_id) = game_label
-        .split_once(' ')
-        .ok_or("missing space in game label")?;
-    let game_id = game_id
-        .parse::<u32>()
-        .context("failed to parse game ID from game label")?;
-
-    let trial_strs = trials.split(';').collect::<Vec<_>>();
-    let mut trials = Vec::new();
-    for (trial_idx, trial) in trial_strs.iter().enumerate() {
-        trials.push(Trial::parse(game_id, trial_idx, trial)?);
-    }
-
-    let game = Game { id: game_id, trials };
-    Ok(game)
-}
 mod part1 {
     use super::*;
 
-    fn part1(input: &str) -> Result<()> {
-        for line in input.lines() {}
-        writeln!(io::stdout(), "{}", input)?;
+    const NUM_RED_CUBES: u32 = 12;
+    const NUM_GREEN_CUBES: u32 = 13;
+    const NUM_BLUE_CUBES: u32 = 14;
+
+    pub fn part1(input: &str) -> anyhow::Result<()> {
+        let mut answer = 0;
+        for line in input.lines() {
+            let game = Game::parse(line)?;
+            if game.trials.iter().all(|t| {
+                t.red <= NUM_RED_CUBES && t.green <= NUM_GREEN_CUBES && t.blue <= NUM_BLUE_CUBES
+            }) {
+                answer += game.id;
+            }
+        }
+        writeln!(io::stdout(), "{}", answer)?;
         Ok(())
     }
 }
 
-fn part2(input: &str) -> Result<()> {
-    writeln!(io::stdout(), "{}", input)?;
+fn part2(input: &str) -> anyhow::Result<()> {
+    //writeln!(io::stdout(), "{}", input)?;
     Ok(())
 }
